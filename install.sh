3c51f4d0b09d500d367c018b4aa10f7b21e7170d
MATTHIASWINS <mattchalles@gmail.com>
2026-07-22 20:37:03 -0400

    Update and rename prereq.sh to install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ set -e
 sudo add-apt-repository ppa:neovim-ppa/unstable -y
 sudo apt update
 sudo apt install make gcc ripgrep fd-find tree-sitter-cli unzip git xclip neovim -y
+git clone https://github.com/MATTHIASGOLIATH/mc_kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
